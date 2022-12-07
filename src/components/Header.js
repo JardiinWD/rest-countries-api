d95b89891efddb6f85ebed0e8a63c452743248bd
JardiinWD <pecorillaalessandro@gmail.com>
2022-12-07 16:08:55 +0100

    #10 - Created the first input field (Homepage)

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,10 @@ import React, { Fragment } from 'react'
 import { Col, Row } from 'reactstrap'
 import classes from './styles/Header.module.scss'
 import Wrapper from '../utils/Wrapper'
+// Link component
+import { Link } from 'react-router-dom'
+// Icon
+import { BsMoonFill } from 'react-icons/bs'
 
 const Header = () => {
     return (
@@ -12,8 +16,19 @@ const Header = () => {
                 {/* Row */}
                 <Row className={classes.row}>
                     {/* xs='12' lg='12' */}
-                    <Col className={classes.col} xs='12' lg='12'>
-                        Header
+                    <Col className="d-flex justify-content-between align-items-center" xs='12' lg='12'>
+                        {/* Header Title */}
+                        <Link className={classes.link} to='/home'>Where in the world?</Link>
+                        {/* Darkmode toggle */}
+                        <div className={`${classes.toggle}`}>
+                            {/* Icon */}
+                            <span className={classes.moon}>
+                                {/* BsMoonFill */}
+                                <BsMoonFill />
+                            </span>
+                            {/* For now is "hard-coded" */}
+                            <span>Dark Mode</span>
+                        </div>
                     </Col>
                 </Row>
             </Wrapper>
--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+// Reactstrap components
+import { Col, Row } from 'reactstrap'
+import Wrapper from '../utils/Wrapper'
+import classes from './styles/Inputs.module.scss'
+import { BsSearch } from 'react-icons/bs'
+
 
 const Inputs = () => {
     return (
-        <div>
-            Inputs component
-        </div>
+        /* Fragment */
+        <Fragment>
+            {/* Wrapper Component */}
+            <Wrapper className="" >
+                {/* classes.row */}
+                <Row className={classes.row}>
+                    {/* Search Input */}
+                    <Col className={`${classes.col} ${classes.search_col}`} lg='4' xs='12'>
+                        {/* search_button */}
+                        <button className={classes.search_button}>
+                            {/* search_icon */}
+                            <BsSearch className={classes.search_icon} />
+                        </button>
+                        {/* Input */}
+                        <div className={classes.search_input}>
+                            {/* Input field */}
+                            <input type="text" placeholder='Search for a country...' />
+                        </div>
+                    </Col>
+                    {/* Dropdown Menu */}
+                    <Col className={classes.col} lg='2'>B</Col>
+                </Row>
+            </Wrapper>
+        </Fragment>
     )
 }
 
